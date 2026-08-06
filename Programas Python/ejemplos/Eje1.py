3796afae12688b08f1b20ae866d888d0ffa5cde9
PrinceSolis <psolisy@miumg.edu.gt>
2026-08-05 18:42:13 -0600

    limpieza de archivos y carga del cuadernillo

--- Programas Python/ejemplos/Eje1.py
+++
@@ -1,7 +0,0 @@
-print ("proramacion II con python")
-print ("proyecto integrado: helpdesk EDU")
-print ("objetivo: contruir un software de helpdesk para la gestion de tickets de soporte tecnico")
-print ("Autor : Prince Estuardo Solis Yocute")
-
-#crear una lista
-
