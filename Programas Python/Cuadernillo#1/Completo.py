fda4cb10f6b84a2c6100f9807eb0490c6c270db4
PrinceSolis <psolisy@miumg.edu.gt>
2026-08-06 22:31:42 -0600

    Mitad de cuadernillo #1

--- Programas Python/Cuadernillo#1/Completo.py
+++ Programas Python/Cuadernillo#1/Completo.py
@@ -20,6 +20,7 @@ try:
 except ValueError:
     #si el usuario ingresa el tiempo erroneo
     print("Error: El tiempo estimado debe ser un numero valido.")
+    
 #Ejercicio 4: Normalizacion de texto del ticket
 
 categoria_ticket = input("Ingrese la categoria del ticket: ")
@@ -29,3 +30,21 @@ ticket_normalizado = categoria_ticket.strip().upper()
 #mostrar el resultado de la normalizacion
 print(f"Categoria del ticket original: {categoria_ticket}")
 print(f"Categoria del ticket normalizado: {ticket_normalizado}")
+
+#ejercicio 5: validacion de campos obligatorios
+nombre = input("Ingrese su nombre: ").strip()
+correo = input("Ingrese su correo electronico institucional: ").strip()
+descripcion = input("Ingrese la descripcion del problema: ").strip()
+
+#validamos de forma ordenada los campos obligatorios
+if not nombre:
+    print("Error: El campo 'nombre' es obligatorio.")
+else:
+    if not correo:
+        print("Error: El campo 'correo' es obligatorio.") 
+    else:
+        if not descripcion:
+            print("Error: El campo 'descripcion' es obligatorio.") 
+        else:
+            print("ticket registrado") 
+
