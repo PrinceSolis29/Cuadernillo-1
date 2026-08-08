bcf3c6f7c3b4b4a5ee6bb1cb58c289d05584b3a9
PrinceSolis <psolisy@miumg.edu.gt>
2026-08-08 14:05:06 -0600

    avance busqueda de ticket por ID

--- Programas Python/Cuadernillo#1/Ejercicio#15.py
+++ Programas Python/Cuadernillo#1/Ejercicio#15.py
@@ -29,7 +29,7 @@ def registro_ticket(lista_tickets, contador_id):
  # validacion de campos oblogatorios (logica del ejercicio 5)
     if not solicitante or not correo or not descripcion:
         print("Error: Todos los campos son obligatorios.")
-    return contador_id #retorna el contador sin incrementar si hay error
+        return contador_id #retorna el contador sin incrementar si hay error
 # ingreso y validacion de prioridad (logica del ejercicio 6)
     prioridad_ingresada = input("Ingrese la prioridad del ticket (BAJA, MEDIA, ALTA): ")
     prioridad = validar_prioridad(prioridad_ingresada)  
@@ -37,6 +37,26 @@ def registro_ticket(lista_tickets, contador_id):
     while prioridad is None:
         print("Error: Prioridad no válida. Intente de nuevo.")
         prioridad_ingresada = input("Ingrese la prioridad del ticket (BAJA, MEDIA, ALTA): ")
-        prioridad = validar_prioridad(prioridad_ingresada)  
+        prioridad = validar_prioridad(prioridad_ingresada) 
         
-    
+#Empaquetado todos los datos ingresados en el diccionario
+    nuevo_ticket = {
+    "id": contador_id,
+    "solicitante": solicitante,
+    "correo": correo,
+    "descripcion": descripcion,
+    "prioridad": prioridad
+    }
+    lista_tickets.append(nuevo_ticket)
+    print(f"-> Ticket registrado con éxito para {solicitante} con ID: {contador_id}")
+    return contador_id + 1 #incrementamos el contador para el siguiente ticket
+def listar_tickets(lista_tickets):
+    print("\n Opción 2: Listado de Tickets ")
+    if len(lista_tickets) == 0:
+        print("No hay tickets registrados.")
+    else:
+        print(f"Total de tickets registrados: {len(lista_tickets)}")
+        for ticket in lista_tickets:
+            #mostramos el reporte con los tickets ingresados
+            print(f"ID: {ticket['id']}, Solicitante: {ticket['solicitante']}, Correo: {ticket['correo']}, Descripción: {ticket['descripcion']}, Prioridad: {ticket['prioridad']}")
+            print("-" * 50)  # Separador visual entre tickets
